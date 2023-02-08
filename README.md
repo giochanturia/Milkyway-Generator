b3ee683d03c2854d6b27092e99023528b7f22368
Gio Chanturia <giochanturia@github.com>
2023-02-08 10:52:51 +0100

    Updated README, added explanation. Switched the main language to English.

--- README.md
+++ README.md
@@ -1,5 +1,104 @@
 # Milkyway-Generator
 
-Just run
+## Work strategy
 
-```python3 -m http.server 8000```
+The work is divided in two conceptual parts: that of the `problem designer` and `programmer`.
+
+The `problem designer`:
+- chooses a problem in accordance with the course syllabus;
+- draws visualization of the problem (`DIAGRAM`);
+- defines and explains any needs for interactive behaviour on the `DIAGRAM`;
+- lists all the variables that need to be randomly generated (`INPUT-NUMBERS`);
+- lists all the variables that are derived from the input numbers and states their dependence (`DERIVED-NUMBERS`);
+- gives the equation for the solution of the problem (`RESULT`);
+- composes the text of the problem (`PROBLEM-TEXT`);
+- composes the text of the answer (`ANSWER-TEXT`);
+- (optionally) composes the explanation (`EXPLANATION-TEXT`).
+
+<img src="example/S1.jpg" width="500px">
+
+Accordingly the `programmer`:
+- initializes the `INPUT-NUMBERS` randomly;
+- defines a function to calculate `DERIVED-NUMBERS` and the `RESULTS`;
+- draws the `DIAGRAM` using the above generated values;
+- adds interactive behaviour to the `DIAGRAM` (events and listeners);
+- generates strings for `PROBLEM-TEXT`, `ANSWER-TEXT`(, `EXPLANATION-TEXT`);
+- defines a function to update the `DIAGRAM` and all the `TEXTS` when the numbers are updated (either when the user requests new numbers using the button or when an event is fired on the `DIAGRAM`).
+
+<img src="example/S2.jpg" width="500px">
+
+`Problem` is the base class for every specific problem. By default, it has:
+- `Problem.parent_selector` -- a CSS selector for the parent object (e.g. `"#container"`);
+- `Problem.problem_number` -- this defines enumeration if the problem is used in a quiz;
+- `Problem.problem_div` -- container of the problem (`<div>`);
+- `Problem.title` -- title object for the problem (`<h4>`);
+- `Problem.new_numbers_button` -- button for the user to generate new numbers (`<button>`);
+- `Problem.new_numbers()` -- function to be fired when the new numbers button is pressed (`void`);
+- `Problem.diagram` -- the diagram of the problem (an instance of the `Diagram` class);
+- `Problem.text` -- problem text object (`<p>`);
+- `Problem.answer` -- answer text object (`<p>`);
+- `Problem.explanation` -- explanation text object (`<p>`);
+
+Any specific problem extends the `Problem` class as follows:
+
+```javascript
+class MyAwesomeProblem extends Problem {
+	constructor(...) {
+		super(...);
+		this.randomize_and_calculate();
+		this.draw_diagram();
+		this.update_text();
+	}
+
+    randomize_and_calculate() {
+        // this function should randomize the input
+        // and calculate the answer using new numbers
+    }
+
+    draw_diagram() {
+        // this function should draw the diagram
+    }
+
+    update_diagram() {
+        // this function should update the diagram
+        // when the variables change
+    }
+
+    update_text() {
+        // this function should update the texts:
+        // problem-text, answer-text(, explanation-text)
+    }
+
+    new_numbers() {
+        // this function should reset the numbers and
+        // everything on the screen... typically, it looks like this
+
+        this.randomize_and_calculate();
+        this.update_diagram();
+        this.update_text();
+    }
+}
+```
+
+The problem then appears in the `HTML` simply by adding
+
+```javascript
+p1 = new MyAwesomeProblem("#container");
+```
+
+to the script.
+
+
+<hr>
+
+## Technical details
+
+### Server execution
+
+The generation is happening 100% on the client side. Consequently, there are no strict requirements from the server. To run a simple `http` server locally, one can use `python` as
+
+```powershell
+python -m http.server 8000
+```
+
+(alternatively, you can use your favourite server software).
--- scripts.js
+++ scripts.js
@@ -3,19 +3,19 @@
 // - - - - - - - - - - - - - - - - - - - - - - - - -
 
 function getRandomInt(max, min = 1) {
-    return min + Math.floor(Math.random() * (max - min + 1));
+	return min + Math.floor(Math.random() * (max - min + 1));
 }
 
 function gcd(x, y) {
-    if (typeof x !== "number" || typeof y !== "number") return false;
-    x = Math.abs(x);
-    y = Math.abs(y);
-    while (y) {
-        var t = y;
-        y = x % y;
-        x = t;
-    }
-    return x;
+	if (typeof x !== "number" || typeof y !== "number") return false;
+	x = Math.abs(x);
+	y = Math.abs(y);
+	while (y) {
+		var t = y;
+		y = x % y;
+		x = t;
+	}
+	return x;
 }
 
 // - - - - - - - - - - - - - - - - - - - - - - - - -
@@ -25,81 +25,84 @@ function gcd(x, y) {
 var problem_counter = 0;
 
 class Diagram {
-    constructor(
-        parent_selector,
-        width = 600,
-        height = 400,
-        padding = 20,
-        bg = "#EEEEEE"
-    ) {
-        this.parent_selector = parent_selector;
-        this.width = width;
-        this.height = height;
-        this.padding = padding;
-        this.bg = bg;
-        this.center = { x: this.width / 2, y: this.height / 2 };
+	constructor(
+		parent_selector,
+		width = 600,
+		height = 400,
+		padding = 20,
+		bg = "#EEEEEE"
+	) {
+		this.parent_selector = parent_selector;
+		this.width = width;
+		this.height = height;
+		this.padding = padding;
+		this.bg = bg;
+		this.center = { x: this.width / 2, y: this.height / 2 };
 
-        this.svg_object = d3
-            .select(this.parent_selector)
-            .append("div")
-            .attr("id", "svg-container")
-            .append("svg")
-            .attr("id", "diagram")
-            .attr("width", this.width)
-            .attr("height", this.height)
-            .attr("viewBox", `0 0 ${this.width} ${this.height}`);
+		this.svg_object = d3
+			.select(this.parent_selector)
+			.append("div")
+			.attr("id", "svg-container")
+			.append("svg")
+			.attr("id", "diagram")
+			.attr("width", this.width)
+			.attr("height", this.height)
+			.attr("viewBox", `0 0 ${this.width} ${this.height}`);
 
-        this.svg_object
-            .append("rect")
-            .attr("x", 0)
-            .attr("y", 0)
-            .attr("width", this.width)
-            .attr("height", this.height)
-            .attr("fill", this.bg);
-    }
+		this.svg_object
+			.append("rect")
+			.attr("x", 0)
+			.attr("y", 0)
+			.attr("width", this.width)
+			.attr("height", this.height)
+			.attr("fill", this.bg);
+	}
 }
 
 class Problem {
-    constructor(
-        parent_selector,
-        diagram_width = 600,
-        diagram_height = 400,
-        diagram_padding = 20,
-        diagram_bg = "#EEEEEE"
-    ) {
-        this.parent_selector = parent_selector;
+	constructor(
+		parent_selector,
+		diagram_width = 600,
+		diagram_height = 400,
+		diagram_padding = 20,
+		diagram_bg = "#EEEEEE"
+	) {
+		this.parent_selector = parent_selector;
 
-        problem_counter += 1;
-        this.problem_number = problem_counter;
+		problem_counter += 1;
+		this.problem_number = problem_counter;
 
-        this.problem_div = d3
-            .select(this.parent_selector)
-            .append("div")
-            .attr("id", `problem-${this.problem_number}`)
-            .attr("class", "problem");
+		this.problem_div = d3
+			.select(this.parent_selector)
+			.append("div")
+			.attr("id", `problem-${this.problem_number}`)
+			.attr("class", "problem");
 
-        this.title = this.problem_div
-            .append("h4")
-            .attr("class", "problem-title")
-            .text(`ამოცანა #${this.problem_number} `);
+		this.title = this.problem_div
+			.append("h4")
+			.attr("class", "problem-title")
+			.text(`Problem #${this.problem_number} `);
 
-        this.new_numbers_button = this.title
-            .append("button")
+		this.new_numbers_button = this.title
+			.append("button")
 			.attr("class", "btn btn-danger")
-            .text("ახალი რიცხვები")
-            .on("click", this.new_numbers.bind(this));
+			.text("New numbers")
+			.on("click", this.new_numbers.bind(this));
 
-        this.diagram = new Diagram(
-            `#problem-${this.problem_number}`,
-            diagram_width,
-            diagram_height,
-            diagram_padding,
-            diagram_bg
-        );
+		this.diagram = new Diagram(
+			`#problem-${this.problem_number}`,
+			diagram_width,
+			diagram_height,
+			diagram_padding,
+			diagram_bg
+		);
 
-        this.text = this.problem_div.append("p").attr("class", "problem-text");
-        this.answer = this.problem_div.append("p").attr("class", "answer-text");
-    }
+		this.text = this.problem_div.append("p").attr("class", "problem-text");
+		this.answer = this.problem_div.append("p").attr("class", "answer-text");
+		this.explanation = this.problem_div
+			.append("p")
+			.attr("class", "explanation-text");
+	}
 
 	new_numbers() {
 		return;
@@ -111,103 +114,102 @@ class Problem {
 // - - - - - - - - - - - - - - - - - - - - - - - - -
 
 class TriangleArea extends Problem {
-    constructor(
-        parent_selector,
-        diagram_width = 600,
-        diagram_height = 400,
-        diagram_padding = 20,
-        diagram_bg = "#EEEEEE"
-    ) {
-        super(
-            parent_selector,
-            diagram_width,
-            diagram_height,
-            diagram_padding,
-            diagram_bg
-        );
+	constructor(
+		parent_selector,
+		diagram_width = 600,
+		diagram_height = 400,
+		diagram_padding = 20,
+		diagram_bg = "#EEEEEE"
+	) {
+		super(
+			parent_selector,
+			diagram_width,
+			diagram_height,
+			diagram_padding,
+			diagram_bg
+		);
 
-        this.randomize();
-        this.draw_diagram();
-        this.update_text();
-    }
+		this.randomize();
+		this.draw_diagram();
+		this.update_text();
+	}
 
-    randomize() {
-        this.rect_width = getRandomInt(this.diagram.width / 2, 100);
-        this.rect_height = getRandomInt(this.diagram.height / 2, 100);
-        this.triangle_vertex = getRandomInt(this.rect_width - 20, 20);
-    }
+	randomize() {
+		this.rect_width = getRandomInt(this.diagram.width / 2, 100);
+		this.rect_height = getRandomInt(this.diagram.height / 2, 100);
+		this.triangle_vertex = getRandomInt(this.rect_width - 20, 20);
+	}
 
-    draw_diagram() {
-        this.rectangle = this.diagram.svg_object
-            .append("rect")
-            .attr("x", this.diagram.center.x - this.rect_width / 2)
-            .attr("y", this.diagram.center.y - this.rect_height / 2)
-            .attr("width", this.rect_width)
-            .attr("height", this.rect_height)
-            .attr("class", "shape-teal");
+	draw_diagram() {
+		this.rectangle = this.diagram.svg_object
+			.append("rect")
+			.attr("x", this.diagram.center.x - this.rect_width / 2)
+			.attr("y", this.diagram.center.y - this.rect_height / 2)
+			.attr("width", this.rect_width)
+			.attr("height", this.rect_height)
+			.attr("class", "shape-teal");
 
-        this.triangle = this.diagram.svg_object
-            .append("polygon")
-            .attr("class", "shape-black")
-            .attr(
-                "points",
-                `${this.diagram.center.x - this.rect_width / 2},${
-                    this.diagram.center.y + this.rect_height / 2
-                } ${this.diagram.center.x + this.rect_width / 2},${
-                    this.diagram.center.y + this.rect_height / 2
-                } ${
-                    this.diagram.center.x -
-                    this.rect_width / 2 +
-                    this.triangle_vertex
-                },${this.diagram.center.y - this.rect_height / 2}`
-            );
-    }
+		this.triangle = this.diagram.svg_object
+			.append("polygon")
+			.attr("class", "shape-red")
+			.attr(
+				"points",
+				`${this.diagram.center.x - this.rect_width / 2},${
+					this.diagram.center.y + this.rect_height / 2
+				} ${this.diagram.center.x + this.rect_width / 2},${
+					this.diagram.center.y + this.rect_height / 2
+				} ${
+					this.diagram.center.x - this.rect_width / 2 + this.triangle_vertex
+				},${this.diagram.center.y - this.rect_height / 2}`
+			);
+	}
 
-    update_diagram() {
-        this.rectangle
-            .attr("x", this.diagram.center.x - this.rect_width / 2)
-            .attr("y", this.diagram.center.y - this.rect_height / 2)
-            .attr("width", this.rect_width)
-            .attr("height", this.rect_height);
+	update_diagram() {
+		this.rectangle
+			.attr("x", this.diagram.center.x - this.rect_width / 2)
+			.attr("y", this.diagram.center.y - this.rect_height / 2)
+			.attr("width", this.rect_width)
+			.attr("height", this.rect_height);
 
-        this.triangle.attr(
-            "points",
-            `${this.diagram.center.x - this.rect_width / 2},${
-                this.diagram.center.y + this.rect_height / 2
-            } ${this.diagram.center.x + this.rect_width / 2},${
-                this.diagram.center.y + this.rect_height / 2
-            } ${
-                this.diagram.center.x -
-                this.rect_width / 2 +
-                this.triangle_vertex
-            },${this.diagram.center.y - this.rect_height / 2}`
-        );
-    }
+		this.triangle.attr(
+			"points",
+			`${this.diagram.center.x - this.rect_width / 2},${
+				this.diagram.center.y + this.rect_height / 2
+			} ${this.diagram.center.x + this.rect_width / 2},${
+				this.diagram.center.y + this.rect_height / 2
+			} ${this.diagram.center.x - this.rect_width / 2 + this.triangle_vertex},${
+				this.diagram.center.y - this.rect_height / 2
+			}`
+		);
+	}
 
-    update_text() {
-        this.text.text(
-            `მართკუთხედის სიგანეა ${
-                this.rect_width
-            } სანტიმეტრი, ხოლო სიმაღლეა ${
-                this.rect_height
-            } სანტიმეტრი. მართკუთხედში ჩახაზულია სამკუთხედი ისე, როგორც დიაგრამაზეა მოცემული. სამკუთხედის წვერო მართკუთხედის გვერდს ჰყოფს ${
-                this.triangle_vertex
-            } და ${
-                this.rect_width - this.triangle_vertex
-            } სანტიმეტრის ზომის მონაკვეთებად. გამოთვალეთ სამკუთხედის ფართობი.`
-        );
-        this.answer.text(
-            `პასუხი: ${
-                (this.rect_width * this.rect_height) / 2
-            } კვადრატული სანტიმეტრი.`
-        );
-    }
+	update_text() {
+		this.text.text(
+			`The width of the rectangle is ${
+				this.rect_width
+			} cm, while its height is ${
+				this.rect_height
+			} cm. A triangle is placed in the rectangle as on the diagram. The vertex of the triangle splits the edge of the rectangle in two segments of length ${
+				this.triangle_vertex
+			} cm and ${
+				this.rect_width - this.triangle_vertex
+			} cm. Calculate the area of the triangle.`
+		);
+		this.answer.text(
+			`Answer: ${
+				(this.rect_width * this.rect_height) / 2
+			} quadratic centimeters.`
+		);
+		this.explanation.text(
+			"Explanation: The area of a triangle is its base multiplied by its height halved. Since both the base and the height of the triangle are equal to the base (width) and the height of the rectangle, respectively, the area will be exatly half the area of the rectangle."
+		);
+	}
 
-    new_numbers() {
-        this.randomize();
-        this.update_diagram();
-        this.update_text();
-    }
+	new_numbers() {
+		this.randomize();
+		this.update_diagram();
+		this.update_text();
+	}
 }
 
 p1 = new TriangleArea("#container");
