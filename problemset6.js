b1d2d3d8085f26d9b00fc0caeaade2efba67c553
Elene Meskhi <elllene@github.com>
2023-04-01 22:55:54 +0200

    commit

--- problemset6.js
+++ problemset6.js
@@ -7,13 +7,37 @@ randomize_and_calculate() {
     this.e = base.getRandomstr(strarray);
     if ( e =='წითელი') {
         this.d = base.getRandomInt(b+c,1);
+        this.p = 1 - ((factorial(b+c))*(factorial(a+b+c+-d)))/((factorial(a+b+c))*(factorial(b+c-d)));
     }
     if ( e=='ყვითელი'){
         this.d = base.getRandomInt(a+c,1);
+        this.p = 1 - ((factorial(a+c))*(factorial(a+b+c+-d)))/((factorial(a+b+c))*(factorial(a+c-d)));
     }
     if (e=='ლურჯი'){
         this.d = base.getRandomInt(a+b,1);
+        this.p = 1 - ((factorial(b+a))*(factorial(a+b+c+-d)))/((factorial(a+b+c))*(factorial(b+a-d)));
     }
     
-    //to be continued
-}
+} 
+    update_text() {
+		this.problem.text(
+			`ყუთში მოთავსებულია ${
+				this.a
+			} წითელი, ${
+				this.b
+			} ყვითელი და  ${
+				this.c
+			} ლურჯი ბურთი. შემთხვევითობის პრინციპით ამოიღეს ${
+				this.d
+			} ბურთი. რა არის იმის ალბათობა, რომ ამოღებული ბურთებიდან ერთი მაინც იქნება ${
+				this.e
+			} ?`
+		);
+		this.answer.text(
+			`პასუხი: ${
+				this.p
+			} .`
+		);
+		
+	}
+   
