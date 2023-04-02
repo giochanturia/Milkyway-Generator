768993801e6e96f4973e66ebd5718b484c092f06
Elene Meskhi <elllene@github.com>
2023-04-02 19:10:45 +0200

    commit

--- base.js
+++ base.js
@@ -18,10 +18,7 @@ export function gcd(x, y) {
 	return x;
 }
 
-export function getRandomstr(strarray) {
-	max = length(strarray);
-	return strarray[Math.floor(Math.random() * max)];
-}
+
 
 export function distanceBetweenPoints(p1, p2) {
 	return Math.sqrt(Math.pow(p2.x - p1.x, 2) + Math.pow(p2.y - p1.y, 2));
