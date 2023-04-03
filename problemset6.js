c24b192d2641f63e31efd2ee6460b4447660b8bc
Elene Meskhi <elllene@github.com>
2023-04-03 04:28:08 +0200

    commit

--- problemset6.js
+++ problemset6.js
@@ -1,5 +1,25 @@
 import * as base from "./base.js";
-randomize_and_calculate();{
+export class Probability extends base.Problem {
+	constructor(
+		parent_selector,
+		problem_number,
+		diagram_width = 600,
+		diagram_height = 400,
+		diagram_padding = 20,
+		diagram_bg = "#EEEEEE"
+	) {
+		super(
+			parent_selector,
+			problem_number,
+			diagram_width,
+			diagram_height,
+			diagram_padding,
+			diagram_bg
+		);
+
+		this.diagram.svg_object.attr("style", "display:none;");
+	}
+randomize_and_calculate() {
     this.a = base.getRandomInt(20, 6);
     this.b = base.getRandomInt(25, 4);
     this.c = base.getRandomInt(30,8);
@@ -19,7 +39,7 @@ randomize_and_calculate();{
     }
     
 } 
-    update_text(); {
+    update_text() {
 		this.question.text(
 			`ყუთში მოთავსებულია $${this.a}$ წითელი, $${this.b}$ ყვითელი და  $${this.c}$ ლურჯი ბურთი. 
 			შემთხვევითობის პრინციპით ამოიღეს $${this.d}$ ბურთი. რა არის იმის ალბათობა, 
@@ -30,8 +50,8 @@ randomize_and_calculate();{
 		);
 			this.explanation.text(`პასუხის საპოვნელად განვიხილოთ ის შემთხვევა, რომელშიც არც ერთი ამოღებული ბურთი სასურველი ფერის არაა
 			და ერთს გამოვაკლოთ ამ შედეგის მიღების ალბათობა.`);
-			
+            
+            jqMath.parseMath(document.body);	
 		}
-		
-	
+    }   
    
